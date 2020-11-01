14a58591f0901254133436252122434388611f14
fredes <fredes@pop-os.localdomain>
2020-11-01 01:58:30 -0300

    feat: add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const cors = require('cors')
 const JokeController = require('./JokeController')
 const CacheService = require('./cacheService')
 require('universal-fetch')
 
 const app = express()
+
+app.use(cors())
 
 const jokeController = new JokeController()
 app.get('/', jokeController.get)
